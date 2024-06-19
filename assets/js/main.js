969c03edd9fd89288e62de257350726a712b3d4b
Mateusz Madej <matmadproject@gmail.com>
2024-06-19 22:23:54 +0200

    loading quest content to view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,37 @@
 import '../sass/main.scss'
+import loadedQuestions from '../json/test-questions.json';
+'use strict'
+
+console.log(loadedQuestions);
+const questionRef = document.getElementById("question");
+const answerARef = document.getElementById("answer-a");
+const answerBRef = document.getElementById("answer-b");
+const answerCRef = document.getElementById("answer-c");
+const answerDRef = document.getElementById("answer-d");
+const answerERef = document.getElementById("answer-e");
+
+
+async function loadQuestions() {
+    try {
+        const response = await fetch('assets/json/test-questions.json');
+        console.log(response);
+        loadedQuestions = await response.json();
+    } catch (error) {
+        console.error('Error loading questions:', error);
+    }
+}
+
+const loadQuestionContentToView = (question) => {
+    console.log(question);
+    questionRef.textContent = question.question;
+    answerARef.textContent = question.answerA;
+    answerBRef.textContent = question.answerB;
+    answerCRef.textContent = question.answerC;
+    answerDRef.textContent = question.answerD;
+    answerERef.textContent = question.answerE;
+}
+
+loadQuestionContentToView(loadedQuestions[0]);
 
 function startTimer(duration, display) {
     let timer = duration, minutes, seconds;
